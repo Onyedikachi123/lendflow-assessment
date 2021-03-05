32f89c61dcc206ccc0ff1b28fec058b4e28e2989
Onyedikachi Shedrach Erugo <36213804+Onyedikachi123@users.noreply.github.com>
2021-03-06 00:06:30 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <h2>Students</h2>
+    <input
+      class="filter-input"
+      v-model="query"
+      type="text"
+      placeholder="Filter.."
+    />
+    <div v-for="student in query ? filtered : students" :key="student.id">
+      <StudentCard :student="student" />
+    </div>
+  </div>
+</template>
+
+<script>
+import StudentCard from "./components/StudentCard.vue";
+export default {
+  name: "App",
+  components: { StudentCard },
+  data: () => ({
+    students: [],
+    filtered: [],
+    loading: false,
+    query: "",
+  }),
+  created() {
+    this.getStudents();
+  },
+  methods: {
+    async getStudents() {
+      this.loading = true;
+      try {
+        const { data } = await this.$http.get(`/assessment/students`);
+        this.students = data.students;
+        console.log(this.students);
+      } catch (error) {
+        console.log(error);
+      }
+      this.loading = false;
+    },
+    filter() {},
+  },
+  watch: {
+    query(value) {
+      if (value) {
+        this.filtered = this.students.filter((student) => {
+          return `${student.firstName} ${student.lastName}`
+            .toLowerCase()
+            .includes(value.toLowerCase());
+        });
+      } else {
+        this.filtered = [];
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Varela&display=swap");
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Varela", sans-serif;
+  outline: none;
+}
+body {
+  background: #f8f8f8;
+  padding: 0 2rem;
+}
+.container {
+  max-width: 700px;
+  margin: 2rem auto;
+  padding: 2rem;
+  background: #fff;
+  border-radius: 6px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+}
+.filter-input {
+  border: 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 0.8rem 0;
+  background: none;
+  margin: 20px 0 10px;
+  width: 100%;
+  transition: all 0.1s ease-in-out;
+  &:focus,
+  &.active {
+    border-bottom: 2px solid royalblue;
+  }
+}
+</style>
---
+++ src/components/StudentCard.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="student">
+    <img :src="student.pic" alt="" />
+    <div class="body">
+      <h3 class="name">{{ name }}</h3>
+      <p class="company">Company: {{ student.company }}</p>
+      <p class="company">Skil: {{ student.skill }}</p>
+      <p class="company">Average: {{ average }}</p>
+      <div class="collapse" ref="collapse">
+        <ul class="grades">
+          <li v-for="(grade, i) in student.grades" :key="'grade' + i">
+            Test {{ i + 1 }}: {{ grade }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <button @click="handleToggleClick" class="toggle">
+      <span v-if="!expanded">+</span>
+      <span v-else>-</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  props: {
+    student: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    expanded: false,
+  }),
+  mounted() {
+    this.toggle();
+  },
+  methods: {
+    handleToggleClick() {
+      this.expanded = !this.expanded;
+      this.toggle();
+    },
+    toggle() {
+      const { collapse } = this.$refs;
+      if (this.expanded) {
+        collapse.style.height = `${collapse.scrollHeight}px`;
+      } else {
+        collapse.style.height = 0;
+      }
+    },
+  },
+  computed: {
+    name() {
+      return `${this.student.firstName} ${this.student.lastName}`;
+    },
+    average() {
+      const total = this.student.grades.reduce((total, n) => total + +n, 0);
+      return total / this.student.grades.length;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.student {
+  display: flex;
+  align-items: start;
+  margin: 1rem 0;
+  padding: 1rem 0;
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+  .name {
+    font-size: 1rem;
+  }
+  .email {
+    margin-top: 3px;
+  }
+  .body {
+    margin-left: 1rem;
+    flex-grow: 1;
+  }
+  .toggle {
+    padding: 0.3rem 0.5rem;
+    border: 0;
+    border-radius: 4px;
+    background: rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    &:hover {
+      background: rgba(0, 0, 0, 0.2);
+    }
+  }
+  .grades {
+    padding: 1rem 0;
+    li {
+      padding: 0.2rem 0;
+    }
+  }
+  .collapse {
+    transition: all 0.2s ease-in-out;
+    overflow: hidden;
+  }
+}
+</style>
